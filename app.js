165574dd516a87ed3a42d09b0e0a1cac8cf799de
ferhanyigit <220103005@ogr.atu.edu.tr>
2025-05-02 19:16:24 +0300

    create a product deletion API added

--- app.js
+++ app.js
@@ -319,8 +319,35 @@ app.post("/basket", async (req, res, next) => {
         console.log("error!!!", error);
         res.status(500).json({ status: "error", data: "Sepet getirilemedi."})
     }
-})
+});
+app.delete("/basket/:userId/:productId", async (req,res,next) =>{
+    const{userId,productId}= req.params;
+    try {
+        await basketModel.destroy({where: {userId:userId, productId:productId}});
+        const basket = await basketModel.findAll({
+            include: [
+                {
+                    model: productModel,
+                    as: "product"
+                }
+            ],
+            where: { userId: userId }
+        });
 
+        let basketTotal = 0;
+
+        for(const b of basket) {
+            const prodTotal = b.product.price * b.quantity;
+
+            basketTotal += prodTotal;
+        }
+
+        res.status(200).json({ status: "success", data: { basket, basketTotal } });
+    }catch(error){
+        res.status(500).json({status:"error", data:"ürün silinemedi."})
+    }
+
+});
 app.listen(port, () => {
     console.log("Sunucu http://localhost:${port} adresinde çalışıyor");
 });
